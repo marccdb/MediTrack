600ff2ae4099f333dc135ec443b6750d5c6de21d
Marcio Del Bianco <marcio.bianco@gmail.com>
2024-07-31 20:50:54 -0300

    Add initial Frontend skeleton

--- MediTrack.AppHost/Program.cs
+++ MediTrack.AppHost/Program.cs
@@ -1,6 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.MediTrack_API>("meditrack-api");
+builder.AddProject<Projects.MediTrack_Presentation>("meditrack-presentation");
 
 
 builder.Build().Run();
